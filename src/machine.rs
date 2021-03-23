d61a0f5085fcbadcf7b9f54e5bb0f3cffa879a4b
Steven Roose <steven@stevenroose.org>
2021-03-23 15:15:33 +0000

    Update to work with std::future and new Hyper

--- src/machine.rs
+++ src/machine.rs
@@ -1,20 +1,25 @@
-use bytes::Bytes;
-use hyper::{self, Error, Headers, HttpVersion, Method, Request, Response, StatusCode, Uri};
 
-use ::uri::UriExt;
+use bytes::Bytes;
+use hyper::body::Body;
+use hyper::header::HeaderMap;
+use hyper::{self, Method, Request, Response, StatusCode, Uri, Version};
+
+use crate::error::Error;
+use crate::uri::UriExt;
 
 pub(crate) struct StateMachine {
     method: Method,
     uri: Uri,
-    version: HttpVersion,
-    headers: Headers,
-    body: Option<Bytes>,
-    remaining_redirects: usize
+    version: Version,
+    headers: HeaderMap,
+    // This is None while we are still receiving the request body.
+    pub(crate) request_body: Option<Bytes>,
+    remaining_redirects: usize,
 }
 
 pub(crate) enum StateMachineDecision {
     Continue,
-    Return
+    Return,
 }
 
 impl StateMachine {
@@ -23,53 +28,47 @@ impl StateMachine {
             method: req.method().clone(),
             uri: req.uri().clone(),
             version: req.version(),
-            headers: Headers::new(),
-            body: None,
-            remaining_redirects: max_redirects
+            headers: HeaderMap::new(),
+            request_body: None,
+            remaining_redirects: max_redirects,
         };
-        state.headers = ::std::mem::replace(req.headers_mut(), Headers::new());
+        state.headers = ::std::mem::replace(req.headers_mut(), HeaderMap::new());
         state
     }
 
-    pub fn set_body(&mut self, body: Option<Bytes>) {
-        self.body = body;
+    pub fn set_body(&mut self, body: Bytes) {
+        self.request_body = Some(body);
     }
 
-    pub fn create_request<B: From<Bytes>>(&self) -> Request<B> {
-        let mut req = Request::new(self.method.clone(), self.uri.clone());
-        req.set_version(self.version);
-        req.headers_mut().clone_from(&self.headers);
-        if let Some(ref bytes) = self.body {
-            req.set_body(bytes.clone());
-        }
-        req
+    pub fn create_request<B: From<Bytes>>(&self) -> Result<Request<B>, Error> {
+        Ok(Request::builder()
+            .method(self.method.clone())
+            .uri(self.uri.clone())
+            .version(self.version)
+            .body(self.request_body.clone().unwrap_or_default().into())?)
     }
 
-    pub fn handle_response(&mut self, res: &Response) -> Result<StateMachineDecision, Error> {
+    pub fn handle_response(&mut self, res: &Response<Body>) -> Result<StateMachineDecision, Error> {
         match res.status() {
-            StatusCode::MovedPermanently | StatusCode::PermanentRedirect => {
+            StatusCode::MOVED_PERMANENTLY | StatusCode::PERMANENT_REDIRECT => {
                 self.follow_redirect(res)
-            },
-            StatusCode::Found | StatusCode::TemporaryRedirect => {
-                self.follow_redirect(res)
-            },
-            StatusCode::SeeOther => {
-                self.method = Method::Get;
-                self.body = None;
-                self.follow_redirect(res)
-            },
-            _ => {
-                Ok(StateMachineDecision::Return)
             }
+            StatusCode::FOUND | StatusCode::TEMPORARY_REDIRECT => self.follow_redirect(res),
+            StatusCode::SEE_OTHER => {
+                self.method = Method::GET;
+                self.request_body = None;
+                self.follow_redirect(res)
+            }
+            _ => Ok(StateMachineDecision::Return),
         }
     }
 
-    fn follow_redirect(&mut self, res: &Response) -> Result<StateMachineDecision, Error> {
+    fn follow_redirect(&mut self, res: &Response<Body>) -> Result<StateMachineDecision, Error> {
         if self.remaining_redirects == 0 {
             return Ok(StateMachineDecision::Return);
         }
         self.remaining_redirects -= 1;
-        if let Some(location) = res.headers().get::<hyper::header::Location>() {
+        if let Some(location) = res.headers().get(hyper::header::LOCATION) {
             let next = self.uri.compute_redirect(location)?;
             remove_sensitive_headers(&mut self.headers, &next, &self.uri);
             self.uri = next;
@@ -80,11 +79,11 @@ impl StateMachine {
     }
 }
 
-pub fn remove_sensitive_headers(headers: &mut Headers, next: &Uri, previous: &Uri) {
+pub fn remove_sensitive_headers(headers: &mut HeaderMap, next: &Uri, previous: &Uri) {
     if !next.is_same_host(previous) {
-        headers.remove_raw("authorization");
-        headers.remove_raw("cookie");
-        headers.remove_raw("cookie2");
-        headers.remove_raw("www-authenticate");
+        headers.remove("authorization");
+        headers.remove("cookie");
+        headers.remove("cookie2");
+        headers.remove("www-authenticate");
     }
 }
